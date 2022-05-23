d1157e837fb469878ffc4c9c1c0d16ebbee4142e
mgregg99 <mattgregg57@gmail.com>
2022-05-23 13:17:00 -0600

    should be working

--- flask/myapp.py
+++ flask/myapp.py
@@ -1,4 +1,5 @@
-from flask import Flask, render_template
+from crypt import methods
+from flask import Flask, render_template, request
 app = Flask(__name__)
 
 
@@ -8,9 +9,20 @@ def home():
     return render_template('index.html')
 
 
-@app.route("/about")
-def about():
-    return "<h1>About Page</h1>"
+@app.route("/update", methods=['POST'])
+def update():
+    if request.method == 'POST':
+        mid = request.form['mid']
+        status = request.form['status']
+        time = request.form['time']
+        f = open("static/jsonfile.txt", "w")
+        f.write(mid)
+        f.write('\n')
+        f.write(status)
+        f.write('\n')
+        f.write(time)
+        f.close()
+        return "update"
 
 
 if __name__ == '__main__':
