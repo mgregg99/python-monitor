5a0b4548dbdab209d4fc4183be5ec1b458353576
Matthew <mgregg99@byu.edu>
2022-07-13 12:26:07 -0600

    successful

--- dev.py
+++ dev.py
@@ -2,40 +2,60 @@ import requests
 from bs4 import BeautifulSoup
 import json
 
-# Get the data from UK and make the Beautiful soup object
-r = requests.get('http://ut.ls.byu.edu/status/scrape')
-soup = BeautifulSoup(r.content, 'html.parser')
 
-# Get the line from the soup object
-messyLine = ""
-for line in soup.find_all('script'):
-    if str(line).find('window.preloadData') != -1:
-        messyLine = str(line)
 
-# Clean the line
-messyLine = messyLine.replace('window.preloadData = ', '')
-messyLine = messyLine.replace('</script>', '')
-messyLine = messyLine.replace('<script>', '')
+asdf = apiScrape()
 
-# conver the string to a json object
-jsonLine = json.loads(messyLine)
 
-# get the data from the json object
-monitorList = {}
-for line in jsonLine['publicGroupList'][0]['monitorList']:
-    monitorList.update({line['id']: line['name']})
 
-# Grab status from the api
-idList = []
-r = requests.get('http://ut.ls.byu.edu/api/status-page/heartbeat/scrape')
-jsonLine = json.loads(r.content)
 
-# Create a list of the ids
-for line in jsonLine['heartbeatList']:
-    idList.append(line)
+def apiScrape():
+    # Get the data from UK and make the Beautiful soup object
+    r = requests.get('http://ut.ls.byu.edu/status/scrape')
+    soup = BeautifulSoup(r.content, 'html.parser')
 
-for line in idList:
-    print(type(jsonLine['heartbeatList'][line]))
+    # Get the line from the soup object
+    messyLine = ""
+    for line in soup.find_all('script'):
+        if str(line).find('window.preloadData') != -1:
+            messyLine = str(line)
+
+    # Clean the line
+    messyLine = messyLine.replace('window.preloadData = ', '')
+    messyLine = messyLine.replace('</script>', '')
+    messyLine = messyLine.replace('<script>', '')
+
+    # conver the string to a json object
+    jsonLine = json.loads(messyLine)
+
+    # get the data from the json object
+    monitorList = {}
+    for line in jsonLine['publicGroupList'][0]['monitorList']:
+        monitorList.update({int(line['id']): line['name']})
+
+    # Grab status from the api
+    idList = []
+    r = requests.get('http://ut.ls.byu.edu/api/status-page/heartbeat/scrape')
+    jsonLine = json.loads(r.content)
+
+    # Create a list of the ids
+    for line in jsonLine['heartbeatList']:
+        idList.append(int(line))
+
+    statusList = {}
+    for line in idList:
+        statusList.update({int(line): jsonLine['heartbeatList'][str(line)][9]['status']})
+
+    returnList = []
+    for line in statusList:
+        templist = [str(monitorList[line]), str(statusList[line])]
+        returnList.append(templist)
+    
+    return returnList
+
+
+
+    
 
 
 
--- flask/myapp.py
+++ flask/myapp.py
@@ -1,28 +1,71 @@
 from crypt import methods
 from flask import Flask, render_template, request
+import requests
+from bs4 import BeautifulSoup
+import json
+
+def apiScrape():
+    # Get the data from UK and make the Beautiful soup object
+    r = requests.get('http://ut.ls.byu.edu/status/scrape')
+    soup = BeautifulSoup(r.content, 'html.parser')
+
+    # Get the line from the soup object
+    messyLine = ""
+    for line in soup.find_all('script'):
+        if str(line).find('window.preloadData') != -1:
+            messyLine = str(line)
+
+    # Clean the line
+    messyLine = messyLine.replace('window.preloadData = ', '')
+    messyLine = messyLine.replace('</script>', '')
+    messyLine = messyLine.replace('<script>', '')
+
+    # conver the string to a json object
+    jsonLine = json.loads(messyLine)
+
+    # get the data from the json object
+    monitorList = {}
+    for line in jsonLine['publicGroupList'][0]['monitorList']:
+        monitorList.update({int(line['id']): line['name']})
+
+    # Grab status from the api
+    idList = []
+    r = requests.get('http://ut.ls.byu.edu/api/status-page/heartbeat/scrape')
+    jsonLine = json.loads(r.content)
+
+    # Create a list of the ids
+    for line in jsonLine['heartbeatList']:
+        idList.append(int(line))
+
+    statusList = {}
+    for line in idList:
+        statusList.update({int(line): jsonLine['heartbeatList'][str(line)][9]['status']})
+
+    returnList = []
+    for line in statusList:
+        templist = [str(monitorList[line]), str(statusList[line])]
+        returnList.append(templist)
+    
+    return returnList
+
 app = Flask(__name__)
 
 
 @app.route("/")
 @app.route("/home")
 def home():
-    return render_template('index.html')
+    monitorList = apiScrape()
+    upnum = 0
+    downnum = 0
+    for line in monitorList:
+        if line[1] == '1':
+            upnum += 1
+        else:
+            downnum += 1
+
+    return render_template('index.html', monitorList=monitorList, upnum=upnum, downnum=downnum)
 
 
-@app.route("/update", methods=['POST'])
-def update():
-    if request.method == 'POST':
-        mid = request.form['mid']
-        status = request.form['status']
-        time = request.form['time']
-        f = open("static/jsonfile.txt", "w")
-        f.write(mid)
-        f.write('\n')
-        f.write(status)
-        f.write('\n')
-        f.write(time)
-        f.close()
-        return "update"
 
 
 if __name__ == '__main__':
